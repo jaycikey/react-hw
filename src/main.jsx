87916330d6c0f4ebeda8052f8fefcbd47394191b
jaycikey <138725941+jaycikey@users.noreply.github.com>
2024-01-13 10:18:24 -0500

    modernNormalize

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// Імпорт стилів нормалізації
+import 'modern-normalize'
 import {App} from './components/App.jsx'
 import './index.css'
 
