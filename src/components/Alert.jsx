c3b6b01fb6a17a4a380e1700232240f9a0c64a9b
jaycikey <138725941+jaycikey@users.noreply.github.com>
2024-01-13 10:11:01 -0500

    cssModule

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,15 +1,19 @@
 import clsx from "clsx";
-import "./Alert.css";
+// Синтаксис імпорту CSS-модуля
+import css from "./Alert.module.css";
 
 export const Alert = ({ variant, outlined, elevated, children }) => {
   return (
     <p
-      className={clsx("alert", variant, {
-        "is-outlined": outlined,
-        "is-elevated": elevated,
+      className={clsx(css.alert, css[variant], {
+        [css.isOutlined]: outlined,
+        [css.isElevated]: elevated,
       })}
     >
       {children}
     </p>
   );
 };
+
+// Отримуємо об'єкт відповідності імен класів
+console.log(css); //
