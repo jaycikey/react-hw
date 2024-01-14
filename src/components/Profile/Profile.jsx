057fbe0722b39f9ac5f4184c6cadab6069ac5ce6
jaycikey <138725941+jaycikey@users.noreply.github.com>
2024-01-14 10:07:47 -0500

    composes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,19 +1,19 @@
-import './Profile.css';
-import clsx from 'clsx';
+import css from './Profile.module.css';
+// import clsx from 'clsx';
 
-export const Profile = ({pilot : {name, email, avatar, age, active}}) => {
-  const statusClasses = clsx('status', {
-'is-active': active,
-'is-retired': !active,
-  });
-    return <div className='profile'>
-      <img src={avatar} alt={name} 
-      width="180"
-      height="180"
-      />
+export const Profile = ({ pilot: { name, email, avatar, age, active } }) => {
+  // const statusClasses = clsx('status', {
+  //   [css.isActive]: active,
+  //   [css.isRetired]: !active,
+  // });
+  const statusClasses = active ? css.isActive : css.isRetired;
+  return (
+    <div className="profile">
+      <img src={avatar} alt={name} width="180" height="180" />
       <h2>{name}</h2>
       <p>Age: {age}</p>
       <p>Email: {email}</p>
-      <p className={statusClasses}>Status: {active ? 'Active': 'Retired'}</p>
+      <p className={statusClasses}>Status: {active ? 'Active' : 'Retired'}</p>
     </div>
-  }
+  );
+};
