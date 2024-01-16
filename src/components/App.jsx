119948aa3ba25174f436c7fba00a55e8443c43a3
jaycikey <138725941+jaycikey@users.noreply.github.com>
2024-01-16 18:22:32 -0500

    7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 
 export const App = () => {
-  // let clicks = 0;
-  const [clicks, setClicks ] = useState(0);
+const [clicks, setClicks] = useState(0);
+const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = () => {
-    // clicks = clicks + 1;
-    setClicks(clicks + 1);
-  }
-  return <button onClick={handleClick}>Current: {clicks}</button>
+const handleClick = () => {
+  setClicks(clicks + 1);
 };
+
+const handleToggle = () => {
+  setIsOpen(!isOpen);
+};
+
+return (
+  <>
+  <button onClick={handleClick}>Current: {clicks}</button>
+  <button onClick={handleToggle}>{isOpen ? "Hide" : "Show"}</button>
+  {isOpen && <p>Now you can see me!</p>}
+  </>
+)
+}
