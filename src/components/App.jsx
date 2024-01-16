0a0ca9505a6e76ea203d2ebc033f2b5649859237
jaycikey <138725941+jaycikey@users.noreply.github.com>
2024-01-16 17:56:50 -0500

    6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 export const App = () => {
-  let clicks = 0;
+  // let clicks = 0;
+  const [clicks, setClicks ] = useState(0);
 
   const handleClick = () => {
-    clicks = clicks + 1;
+    // clicks = clicks + 1;
+    setClicks(clicks + 1);
   }
   return <button onClick={handleClick}>Current: {clicks}</button>
 };
