48a5552d7b66f4bd7479fcc3535a224303a57a2c
jaycikey <138725941+jaycikey@users.noreply.github.com>
2024-01-20 12:03:13 -0500

    multiEffects

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,24 @@ export const App = () => {
   const [clicks, setClicks] = useState(0);
 
   useEffect(() => {
+    console.log('You caln see me only once!');
+  });
+
+  useEffect(() => {
     console.log('Clicks update: ', clicks);
   }, [clicks]);
+
+  useEffect(() => {
+    document.title = `You clicked ${clicks} times`;
+  });
+  
   return (
-    <button onClick={()=> {setClicks(clicks + 1)}}>You clicked {clicks} times</button>
+    <button
+      onClick={() => {
+        setClicks(clicks + 1);
+      }}
+    >
+      You clicked {clicks} times
+    </button>
   );
 };
