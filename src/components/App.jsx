8a210f407c37965bdf2de91c30bbf1f0879b11b8
jaycikey <138725941+jaycikey@users.noreply.github.com>
2024-01-20 12:51:43 -0500

    localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,32 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const App = () => {
-  const [firts, setFirts] = useState(0);
-  const [second, setSecond] = useState(0);
+  const [clicks, setClicks] = useState(() => {
+    // Зчитуємо значення за ключем
+    const savedClicks = window.localStorage.getItem('saved-clicks');
+
+    //Якщо там щось є, повертажємо це
+    //значення як початкове значення стану
+    if (savedClicks !== null) {
+      // localStorage.getItem(). Метод localStorage зберігає всі дані у вигляді рядків, незалежно від їх первісного типу. Тому, коли ви зберігаєте число, наприклад, кількість натискань кнопки, воно конвертується в рядок перед збереженням. Під час отримання цього значення назад з localStorage, воно все ще є рядком.
+      return parseInt(savedClicks, 10)  // або +savedClicks
+    }
+
+    //У протилежному випадку повертаємо
+    // яке-небудь значення за замовчуванням
+    return 0;
+  });
 
   useEffect(() => {
-    console.log('Firts update: ', firts);
-  }, [firts]);
-
-  useEffect(()=> {
-    console.log("Second update: ", second);
-  }, [second])
-
-  useEffect(()=>{
-    console.log("Firts or second update: ", firts + second)
-  }, [firts, second])
+    window.localStorage.setItem('saved-clicks', clicks);
+  }, [clicks]);
 
   return (
-    <>
-    <button onClick={() => setFirts(firts + 1)}> Firts: {firts}</button>
-    <button onClick={() => setSecond(second + 1)}> Second: {second}</button>
-    </>
+    <div>
+      <button onClick={() => setClicks(clicks + 1)}>
+        You clicked {clicks} times
+      </button>
+      <button onClick={() => setClicks(0)}>Reset</button>
+    </div>
   );
 };
