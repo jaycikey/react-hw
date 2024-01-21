5adf7f9ee33c6d748a8f2c3c8ee2421b2425cb75
jaycikey <138725941+jaycikey@users.noreply.github.com>
2024-01-21 08:35:14 -0500

    localStorage

--- src/components/Reader.jsx
+++ src/components/Reader.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Progress } from './Progress';
 import { ArticleView } from './ArticleView';
 import { Controls } from './Controls';
 
 export const Reader = ({ items }) => {
-  const [selectedIdx, setSelectedIdx] = useState(0);
+  const [selectedIdx, setSelectedIdx] = useState(() => {
+    const savedLocalStorage = window.localStorage.getItem('article');
+    return savedLocalStorage !== null ? JSON.parse(savedLocalStorage) : 0;
+  });
 
   const handleNext = () => {
     setSelectedIdx(selectedIdx + 1);
@@ -17,6 +20,10 @@ export const Reader = ({ items }) => {
   const selectedArticle = items[selectedIdx];
   const isNextDisabled = selectedIdx === items.length - 1;
   const isPrevDisabled = selectedIdx === 0;
+
+  useEffect(() => {
+    window.localStorage.setItem('article', JSON.stringify(selectedIdx));
+  },[selectedIdx]);
 
   return (
     <div>
