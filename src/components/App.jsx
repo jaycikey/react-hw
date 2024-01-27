f220f2d941d130cdc34e53898534afaa8bad474f
jaycikey <138725941+jaycikey@users.noreply.github.com>
2024-01-27 11:41:28 -0500

    5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { LangSwitcher } from './LangSwitcher';
 import { LoginForm } from './LoginForm';
 import { Mycomponent } from './MyComponent';
 import { SearchBar } from './SearchBar';
@@ -6,12 +8,16 @@ export const App = () => {
   const handleLogin = userData => {
     console.log(userData);
   };
+
+  const [lang, setLang] = useState('uk');
   return (
     <div>
-      {/* <h1>Please login to your account!</h1>
+      <h1>Please login to your account!</h1>
       <LoginForm onSubmit={handleLogin}/>
-      <Mycomponent /> */}
+      <Mycomponent /> 
       <SearchBar/>
+      <p>Selected language: {lang}</p>
+      <LangSwitcher value={lang} onSelect={setLang} />
     </div>
   );
 };
