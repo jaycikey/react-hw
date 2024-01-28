a9357aec1d1fb37c54a50fbc1df29f7c91bb02fc
jaycikey <138725941+jaycikey@users.noreply.github.com>
2024-01-28 15:21:10 -0500

    str

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,25 @@
 import { useState } from 'react';
-import { Filter } from './Filter';
-import { Users } from './Users';
-import { UserForm } from './UserForm';
+import { Filter } from './Filter/Filter';
+import { Users } from './Users/Users';
+import { UserForm } from './UserForm/UserForm';
+
+const initialUsers = [
+  { username: 'Jacob', access: 'r', id: 11124 },
+  { username: 'Mango', access: 'w', id: 89278 },
+  { username: 'Elena', access: 'r', id: 78817 },
+  { username: 'Orlando', access: 'm', id: 87667 },
+  { username: 'Gimli', access: 'w', id: 45776 },
+];
 
 export const App = () => {
   const [nameFilter, setnameFilter] = useState('');
-  const [users, setUsers] = useState([
-    { username: 'Jacob', id: 10001 },
-    { username: 'Mango', id: 10002 },
-    { username: 'Elena', id: 10003 },
-    { username: 'Orlando', id: 10004 },
-    { username: 'Gimli', id: 10005 },
-  ]);
+  const [users, setUsers] = useState(initialUsers);
+
+  const addUser = newUser => {
+    setUsers(prevUsers => {
+      return [...prevUsers, newUser];
+    });
+  };
 
   const deleteUser = userId => {
     setUsers(prevUsers => {
@@ -19,19 +27,7 @@ export const App = () => {
     });
   };
 
-  const addUser = newUser => {
-    setUsers(prevUser => {
-      return [
-        ...prevUser,
-        {
-          username: newUser,
-          id: Date.now(),
-        },
-      ];
-    });
-  };
-
-  const visibalUser = users.filter(user =>
+  const visibleUsers = users.filter(user =>
     user.username.toLowerCase().includes(nameFilter.toLowerCase())
   );
 
@@ -40,7 +36,7 @@ export const App = () => {
       <UserForm onAdd={addUser} />
       <hr />
       <Filter value={nameFilter} onChange={setnameFilter} />
-      <Users items={visibalUser} onDelete={deleteUser} />
+      <Users items={visibleUsers} onDelete={deleteUser} />
     </>
   );
 };
