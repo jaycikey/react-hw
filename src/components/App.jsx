4e408b53a079c1e9f8e7c90781ba3aca0093e938
jaycikey <138725941+jaycikey@users.noreply.github.com>
2024-02-03 11:13:24 -0500

    5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,35 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { fetchArticlesWithTopic } from '../articles-api.js';
 import { ArticleList } from './ArticleList';
 
 export const App = () => {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [error, setEror] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     async function fetchArticles() {
       try {
-        //1. Встановлюємо індикатор в trey перед запитом
         setLoading(true);
-        const response = await axios.get(
-          'https://hn.algolia.com/api/v1/search?query=react'
-        );
-        setArticles(response.data.hits);
+        // 2. Використовуємо HTTP-функцію
+        const data = await fetchArticlesWithTopic('react');
+        setArticles(data);
       } catch (error) {
-        // Встановлюємо стан error в true
-        setEror(true);
+        setError(true);
       } finally {
-        //2. Встановлюємо індикатор в false після запиту
         setLoading(false);
       }
     }
+
     fetchArticles();
   }, []);
+
   return (
     <div>
       <h1>Latest articles</h1>
       {loading && <p>Loading data, please wait...</p>}
       {error && (
-        <p>Whoops, somthing went wrong! Please try reloading this page!</p>
+        <p>Whoops, something went wrong! Please try reloading this page!</p>
       )}
       {articles.length > 0 && <ArticleList items={articles} />}
     </div>
