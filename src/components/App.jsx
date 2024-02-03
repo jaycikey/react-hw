6813cf461da94af35344b8dedce9aaf81a15b4b5
jaycikey <138725941+jaycikey@users.noreply.github.com>
2024-02-03 09:42:02 -0500

    firts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,14 @@ import { useEffect } from 'react';
 import axios from 'axios';
 
 export const App = () => {
-  useEffect(()=>{
+  useEffect(() => {
     // 1. Оголошуємо асинхронну функцію
-    async function fetchArticles(){
+    async function fetchArticles() {
       //Тут будемо виконувати HTTP-запит
-
     }
     //2. Викликаємо її одразу після оголошення
-    
-  }, [])
+    fetchArticles();
+  }, []);
   return (
     <div>
       <h1>Latest articles</h1>
