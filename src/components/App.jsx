d2e020d701c8c7aec51d4a734d4c405fa1b88f45
jaycikey <138725941+jaycikey@users.noreply.github.com>
2024-02-03 10:14:27 -0500

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,37 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export const App = () => {
+  // 1. Оголошуемо стан
+  const [articles, setArticles] = useState([]);
+
   useEffect(() => {
-    // 1. Оголошуємо асинхронну функцію
+    // 2. Оголошуємо асинхронну функцію
     async function fetchArticles() {
       //Тут будемо виконувати HTTP-запит
       const response = await axios.get(
         'https://hn.algolia.com/api/v1/search?query=react'
       );
-      console.log(response);
+      //3. Записуемо данні в стан
+      setArticles(response.data.hits);
     }
-    //2. Викликаємо її одразу після оголошення
+    //4. Викликаємо її одразу після оголошення
     fetchArticles();
   }, []);
   return (
     <div>
       <h1>Latest articles</h1>
+      {articles.length > 0 && (
+        <ul>
+          {articles.map(({ objectID, url, title }) => (
+            <li key={objectID}>
+              <a href={url} target="_blank" rel="noreferrer noopener">
+                {title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
